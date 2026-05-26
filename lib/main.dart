0507b307f87344a92bd78924c6557749fea799cb
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-05-26 18:47:27 +0000

    feat: login & sign up screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:twitter_clone/screens/first.dart';
-import 'package:twitter_clone/screens/second.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:twitter_clone/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,88 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.blue)),
       // home: const MyHomePage(title: 'Settings'),
-      routes: {
-        '/': (context) => MyHomePage(title: "Twitter Clone"),
-        '/first': (context) => FirstScreeen(name: "Shuush!"),
-        '/second': (context) => SecondScreen(),
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  final GlobalKey<FormState> _signInKey = GlobalKey();
-  final RegExp emailValid = RegExp(
-    r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            key: _signInKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(hintText: "Enter email"),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter email";
-                    } else if (!emailValid.hasMatch(value)) {
-                      return "Please enter valid email";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(hintText: "Enter password"),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter password";
-                    } else if (value.length < 6) {
-                      return "Please the password should be more than 6 characters";
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_signInKey.currentState!.validate()) {
-                      debugPrint("Email: ${_emailController.text}");
-                      debugPrint("Password: ${_passwordController.text}");
-                    }
-                  },
-                  child: Text("Submit"),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      routes: {'/': (context) => Login()},
     );
   }
 }
---
+++ lib/screens/signup.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/screens/login.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final GlobalKey<FormState> _signInKey = GlobalKey();
+  final RegExp emailValid = RegExp(
+    r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _signInKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Sign up to Twitter",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.fromLTRB(15, 30, 15, 0),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: "Enter email",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter email";
+                  } else if (!emailValid.hasMatch(value)) {
+                    return "Please enter valid email";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  hintText: "Enter password",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter password";
+                  } else if (value.length < 6) {
+                    return "Please the password should be more than 6 characters";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Container(
+              width: 150,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  if (_signInKey.currentState!.validate()) {
+                    debugPrint("Email: ${_emailController.text}");
+                    debugPrint("Password: ${_passwordController.text}");
+                  }
+                },
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(overlayColor: WidgetStateColor.transparent),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Already have an acount? Login up here"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
